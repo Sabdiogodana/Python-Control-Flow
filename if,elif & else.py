decd40589118d243dd4745fef74dd9eacc0f2313
sabdiogodana <sabdiyogodanaj@gmail.com>
2022-05-10 21:47:08 +0300

    if,else

---
+++ if,elif & else.py
@@ -0,0 +1,30 @@
+
+tempreture = 35
+if tempreture > 30:
+    print("It's warm")
+    print("Drink water")
+elif tempreture > 20:
+    print("I'ts nice weather") 
+else:
+    print("I'ts cold")       
+print("Done")
+
+# Building an application for university
+age = 22
+if age >= 18:
+    message = ("Eligible")
+else:
+    message = ("Not eligible") 
+    # assigning values to Variables
+message = "Eligible"  if age >= 18 else "Not eligible"
+print(message)  
+
+# Building an application for processing loans
+highincome = False
+goodcredits =True
+
+if highincome ==True and goodcredits:
+    print("Eligible")
+else:
+    print("Not eligible")  
+
---
+++ if,else.py
@@ -0,0 +1,8 @@
+age = 22
+if age >= 18:
+    message = ("Eligible")
+else:
+    message = ("Not eligible") 
+    # assigning values to Variables
+message = "Eligible"  if age >= 18 else "Not eligible"
+print(message)     
